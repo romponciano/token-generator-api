89343d5d927ade345b864650d51fed2fbfec308b
Rômulo Ponciano <romponciano@gmail.com>
2021-06-17 14:30:15 -0300

    fix token endpoint

--- src/main/java/com/rom/application/TokenController.java
+++ src/main/java/com/rom/application/TokenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tg/{username}/model/{modelId}")
+@RequestMapping("/tg")
 public class TokenController {
 
     @Autowired
