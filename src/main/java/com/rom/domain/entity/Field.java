0913e73bd3ca7f8f2ec8f706609b24d649378bdc
Rômulo Ponciano <romponciano@gmail.com>
2021-07-01 22:57:42 -0300

    add order field to improve field management

--- src/main/java/com/rom/domain/entity/Field.java
+++ src/main/java/com/rom/domain/entity/Field.java
@@ -4,11 +4,13 @@ import java.util.Objects;
 
 public class Field {
     private String name;
+    private Integer order;
     private String type;
     private Object value;
 
-    public Field(String name, String type, Object value) {
+    public Field(String name, Integer order, String type, Object value) {
         this.name = name;
+        this.order = order;
         this.type = type;
         this.value = value;
     }
@@ -40,11 +42,19 @@ public class Field {
         this.value = value;
     }
 
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Field)) return false;
         Field field = (Field) o;
-        return Objects.equals(getName(), field.getName()) && Objects.equals(getType(), field.getType()) && Objects.equals(getValue(), field.getValue());
+        return Objects.equals(getName(), field.getName()) && Objects.equals(getType(), field.getType()) && Objects.equals(getValue(), field.getValue()) && Objects.equals(getOrder(), field.getOrder());
     }
 }
